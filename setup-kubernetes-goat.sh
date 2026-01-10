8f23fdce7b587be329fc6c3baffd39fad5193abc
eric <57949806+wpsec@users.noreply.github.com>
2026-01-10 13:17:21 +0800

    nodeport

--- setup-kubernetes-goat.sh
+++ setup-kubernetes-goat.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Author: Madhu Akula
-# This program has been created as part of Kubernetes Goat
-# Kubernetes Goat setup and manage vulnerable infrastructure
+set -e
 
 INSECURE=""
 KUBECONFIG_ARG=""
 export KUBECTL_INSECURE=""
 export HELM_INSECURE=""
+
+VALUES_FILE="scenarios/metadata-db/values.yaml"
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
@@ -19,7 +19,6 @@ while [[ $# -gt 0 ]]; do
       shift 2
       ;;
     *)
-      echo "unknown arg: $1"
       shift
       ;;
   esac
@@ -34,46 +33,20 @@ if [[ -n "$INSECURE" ]]; then
   export HELM_INSECURE="--kube-insecure-skip-tls-verify"
 fi
 
-# Checking kubectl setup
-kubectl $KUBECTL_INSECURE version > /dev/null 2>&1
-if [ $? -eq 0 ];
-then
-    echo "kubectl setup looks good."
-else 
-    echo "Error: Could not find kubectl or an other error happened, please check kubectl setup."
-    exit;
-fi
+kubectl $KUBECTL_INSECURE version >/dev/null 2>&1 || exit 1
 
-# Deprecated helm2 scenario
-# Checking helm2 setup
-#helm2 --help > /dev/null 2>&1
-#if [ $? -eq 0 ];
-#then
-#    echo "helm2 setup looks good."
-#else
-#    echo "Error: Could not find helm2, please check helm2 setup."
-#    exit;
-#fi
-#
-## helm2 setup
-#echo "setting up helm2 rbac account and initialise tiller"
-# kubectl apply -f scenarios/helm2-rbac/setup.yaml
-#helm2 init --service-account tiller
-
-# wait for tiller service to ready
-# echo "waiting for helm2 tiller service to be active."
-# sleep 50
-
-# deploying insecure-rbac scenario
-echo "deploying insecure super admin scenario"
 kubectl $KUBECTL_INSECURE apply -f scenarios/insecure-rbac/setup.yaml
 
-# deploying helm chart to verify the setup
-echo "deploying helm chart metadata-db scenario"
-helm $HELM_INSECURE install metadata-db scenarios/metadata-db/
+HELM_ARGS=""
+if [ -f "$VALUES_FILE" ]; then
+  HELM_ARGS="-f $VALUES_FILE"
+fi
 
-# setup the scenarios/configurations
-echo 'deploying the vulnerable scenarios manifests'
+helm $HELM_INSECURE upgrade --install metadata-db scenarios/metadata-db \
+  --namespace default \
+  $HELM_ARGS \
+  --wait
+
 kubectl $KUBECTL_INSECURE apply -f scenarios/batch-check/job.yaml
 kubectl $KUBECTL_INSECURE apply -f scenarios/build-code/deployment.yaml
 kubectl $KUBECTL_INSECURE apply -f scenarios/cache-store/deployment.yaml
@@ -84,7 +57,3 @@ kubectl $KUBECTL_INSECURE apply -f scenarios/kubernetes-goat-home/deployment.yam
 kubectl $KUBECTL_INSECURE apply -f scenarios/poor-registry/deployment.yaml
 kubectl $KUBECTL_INSECURE apply -f scenarios/system-monitor/deployment.yaml
 kubectl $KUBECTL_INSECURE apply -f scenarios/hidden-in-layers/deployment.yaml
-
-echo 'Successfully deployed Kubernetes Goat. Have fun learning Kubernetes Security!'
-echo 'Ensure pods are in running status before running access-kubernetes-goat.sh script'
-echo 'Now run the bash access-kubernetes-goat.sh to access the Kubernetes Goat environment.'
